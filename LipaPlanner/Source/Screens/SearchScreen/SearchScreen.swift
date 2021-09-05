459aad192d517dc41e9cd02dac43ac1710d45555
Dmitriy Arhipov <dmitriy.arkhipov.biz@mail.ru>
2021-09-05 14:55:14 +0700

    Заменен root компонент экрана поиска

--- LipaPlanner/Source/Screens/SearchScreen/SearchScreen.swift
+++ LipaPlanner/Source/Screens/SearchScreen/SearchScreen.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SearchScreen: View {
     var body: some View {
-        VStack{
+        ScrollView(.vertical) {
             FiltersView()
             SearchHistoryView()
         }
