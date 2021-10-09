37e7ca4255bc45efcc21846b4e5b36963f8fab3e
Dmitriy Arhipov <dmitriy.arkhipov.biz@mail.ru>
2021-10-09 21:59:23 +0700

    Тестовая реализация навигации в списке

--- LipaPlanner/Source/Screens/TrainsListScreen/TrainListScreen.swift
+++ LipaPlanner/Source/Screens/TrainsListScreen/TrainListScreen.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct TrainListScreen: View {
+    @State var openCard: Bool = false
+    @State var value: String = ""
+    
     var body: some View {
         NavigationView {
             VStack {
                 List {
-                    Text("Hello")
-                    Text("Hello2")
-                    Text("Hello3")
+                    Text("Hello").onTapGesture {
+                        self.openCard = true
+                        self.value = "Hello"
+                    }
+                    Text("Hello2").onTapGesture {
+                        self.openCard = true
+                        self.value = "Hello2"
+                    }
+                    Text("Hello3").onTapGesture {
+                        self.openCard = true
+                        self.value = "Hello3"
+                    }
+                }
+                NavigationLinkFromCode(isActive: self.$openCard) {
+                    Text(self.value)
                 }
             }
         }
---
+++ LipaPlanner/Source/Screens/TrainsListScreen/TrainListScreenViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TrainListScreenViewModel.swift
+//  LipaPlanner
+//
+//  Created by Dmitriy Arkhipov on 09.10.2021.
+//
+
+import Foundation
+
+class TrainListScreenViewModel {
+    
+}
